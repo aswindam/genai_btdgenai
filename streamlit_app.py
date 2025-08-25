391c4181b4729516d2e58d38c7361ebec4792f5c
Aswin Kumar Damarapalli <101462863+aswindam@users.noreply.github.com>
2025-08-25 03:26:50 +0000

    added entry pt

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+# Ensure `app` folder is in the Python path
+sys.path.append(os.path.join(os.path.dirname(__file__), "app"))
+
+# Import and run your Streamlit app
+import show
